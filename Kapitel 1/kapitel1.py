35cce825a27868e7b00aa2595a1b61fe47d99d53
MariaSvard <mariasvard@hotmail.com>
2024-11-07 10:38:25 +0100

    first commit

---
+++ Kapitel 1/kapitel1.py
@@ -0,0 +1,76 @@
+"""
+Skapa och hantera variabler
+
+"""
+
+# name = "Maria"
+# age = 33
+# temperature = 10
+# is_student = True
+# print(name, age, temperature, is_student)
+
+# person = {name: "Maria", age: 33, temperature: 10, is_student: True }
+# print(person)
+
+# name = "Maria"
+# age = 33
+# temperature = 10
+# is_student = True
+# # print("Namn: ", name, ", Ålder: ", age, ", Temperatur idag: ", temperature, ", Är student: ", is_student)
+# print("Namn: ", name)
+# print("Ålder: ", age)
+# print("Temperatur utomhus idag: ", temperature, "grader celcius")
+# print("Är student: ", is_student)
+
+"""
+
+Aritmetiska operationer
+
+"""
+
+# a = 15
+# b = 4
+# results = a + b # addition
+# print("Results is: ", results)
+
+# a = 15
+# b = 4
+# results = a - b # subtraktion
+# print("Results is: ", results)
+
+# a = 15
+# b = 4
+# results = a * b # multiplikation
+# print("Results is: ", results)
+
+# a = 15
+# b = 4
+# results = a / b # division
+# print("Results is: ", results)
+
+# a = 15
+# b = 4
+# results = a // b # golvdivision
+# print("Results is: ", results)
+
+# a = 15
+# b = 4
+# results = a % b # modulus
+# print("Results is: ", results)
+
+# a = 15
+# b = 4
+# results = a ** b # exponentiering
+# print("Results is: ", results)
+
+"""
+
+Stränghantering
+
+"""
+
+# str = "Hello, World!"
+# print(str[0]) # första tecknet i strängen
+# print(str[7:12]) # tecken från position 7 till 12
+# print(str[::2]) # varannat tecken i strängen
+# print(str[::-1]) # strängen baklänges
---
+++ Kapitel 2/kapitel2.py
@@ -0,0 +1,50 @@
+"""
+
+Skapa ett username från ett namn
+
+"""
+
+# namn = " aNnA kaRlSsOn "
+# str = " aNnA kaRlSsOn "
+# print(str.strip()) # tar bort mellanslagen, innan och efter
+# print(str.lower()) # alla bokstäver blir små
+# print(str.title()) # första bokstaven i varje ord blir stor
+# print(namn.replace(" ", "-")) # mellanslaget i namnet ersätts med -
+
+# namn = namn.strip()
+# namn = namn.title()
+# namn = namn.replace(" ", "-")
+# print(namn)
+
+"""
+
+Beställning på ICA
+
+"""
+# empty_tuple = ()
+
+# varor = "bröd", "mjölk", "ägg", "smör", "ost", "yoghurt"
+# print(varor[:3]) # skriver ut de tre första varorna
+# print(varor[4:6]) # skriver ut de sista två varorna
+# print(varor[::2]) # skriver ut varannan vara
+
+"""
+
+Topplista med filmer
+
+"""
+
+# empty_list = []
+
+# movies = ["Inception", "The Matrix", "Interstellar", "The Prestige"]
+# movies.append("Memento") # lägg till filmen Memento i slutet av listan VIKTIG!!
+# print(movies)
+
+# movies[1] = "The Lord of the Rings" # Byt ut filmen "The Matrix" mot "The Lord of the Rings".
+# print(movies)
+
+# movies.remove("The Prestige") # Ta bort filmen "The Prestige" från listan
+# print(movies)
+
+# movies.insert(2, "The Dark Knight") # Sätt in filmen "The Dark Knight" på position 2 i listan
+# print(movies)
